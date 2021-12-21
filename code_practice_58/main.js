6573789371c64044faa0b936a3c546fcd6b0a69f
theazarov <theazarov@gmail.com>
2021-12-21 18:23:18 +0300

    Initial commit.

---
+++ code_practice_58/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let field = document.querySelector('#field');
+console.log('hi world')
+console.log('my check')
+console.log('my check')
+console.log('my check')
+console.log('my check')
+console.log('my check')
+console.log('my check')
+console.log('my check')
